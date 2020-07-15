437f02bd3c9ac52ed4d6eaa6aa24e6790f7ceac5
Alexander Shultz <ashultzy@rams.colostate.edu>
2020-07-14 22:26:22 -0600

    created ImageResponse type

--- response/image.go
+++ response/image.go
@@ -1,15 +1,27 @@
 package response
 
-import "net/url"
+import (
+	"encoding/json"
+	"net/url"
+)
 
 type ImageResponse struct {
-	Height int `json:"height"`
-	URL string `json:"url"`
-	Width int `json:"width"`
+	Height int    `json:"height,int"`
+	URL    string `json:"url"`
+	Width  int    `json:"width"`
+}
+
+func NewImageResponse(data []byte) (*ImageResponse, error) {
+	image := &ImageResponse{}
+	err := json.Unmarshal(data, &image)
+	if err != nil {
+		return nil, err
+	}
+	return image, nil
 }
 
 type Image struct {
 	height int
-	url url.URL
-	width int
+	url    url.URL
+	width  int
 }
